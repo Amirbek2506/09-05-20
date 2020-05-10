6b6c74ad5859aefe90fc5b65ec77b39d1a270706
Amirbek2506 <62469848+Amirbek2506@users.noreply.github.com>
2020-05-10 17:31:55 +0500

    finish второго задача

--- HW-09-05-20/Program.cs
+++ HW-09-05-20/Program.cs
@@ -8,6 +8,9 @@ namespace HW_09_05_20
         static void Main(string[] args)
         {
             //Задача №1
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\t\tЗадача №1");
+            Console.ResetColor();
             var Number = new Random().Next(10, 100000000).ToString();
             int[] ArrDigits = new int[Number.Length];
             Console.WriteLine(Number);
@@ -17,6 +20,20 @@ namespace HW_09_05_20
             }
             foreach (int Digits in ArrDigits.Reverse())
                 Console.Write($"{Digits}, ");
+            Console.ReadKey();
+            //Задача №2
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n\t\tЗадача №2");
+            Console.ResetColor();
+            int[] InputArr= new int[] { 1, 2, 3, 4, 5, -1, -2, -3, -14, -5 };
+            int[] OutputArr = new int[2];
+            OutputArr[0] = InputArr.Where(Num=>Num>0).Count();
+            OutputArr[1] = InputArr.Where(Num => Num < 0).Sum();
+            //вводим на экран
+            Console.Write("InputArrey = ");
+            foreach (int Num in InputArr)Console.Write($"{Num}, ");
+            Console.Write("\nOutputArrey = ");
+            foreach (int Num in OutputArr) Console.Write($"{Num}, ");
             Console.ReadKey();
         }
     }
