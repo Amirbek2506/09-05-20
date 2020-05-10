6ebef69334a9dcff6f4035a78595d2e4746bfdcf
Amirbek2506 <62469848+Amirbek2506@users.noreply.github.com>
2020-05-10 14:30:29 +0500

    создать проект

---
+++ HW-09-05-20/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_09_05_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
