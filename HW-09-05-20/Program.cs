27677238e997b19706f3c63b09d84a01fb80f55a
Amirbek2506 <62469848+Amirbek2506@users.noreply.github.com>
2020-05-10 16:44:58 +0500

    finish первая задача

--- HW-09-05-20/Program.cs
+++ HW-09-05-20/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace HW_09_05_20
 {
@@ -6,7 +7,17 @@ namespace HW_09_05_20
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Задача №1
+            var Number = new Random().Next(10, 100000000).ToString();
+            int[] ArrDigits = new int[Number.Length];
+            Console.WriteLine(Number);
+            for (int i = 0; i < Number.Length; i++)
+            {
+                ArrDigits[i] = int.Parse((Number.Skip(i).Take(1)).First().ToString());
+            }
+            foreach (int Digits in ArrDigits.Reverse())
+                Console.Write($"{Digits}, ");
+            Console.ReadKey();
         }
     }
 }
