c9f4571126bffcb1979cb6590e24468bace12c4e
Amirbek2506 <62469848+Amirbek2506@users.noreply.github.com>
2020-05-12 18:46:44 +0500

    Fixed kod

--- HW-09-05-20/Program.cs
+++ HW-09-05-20/Program.cs
@@ -9,42 +9,33 @@ namespace HW_09_05_20
         {
             //Задача №1
             Console.ForegroundColor = ConsoleColor.Green;Console.WriteLine("\t\tЗадача №1");Console.ResetColor();
-            var Number = 987654321.ToString().ToCharArray();
-            foreach (var Digits in Number.Reverse())
-                Console.Write($"{Digits}, ");
+            foreach (var Digits in 987654321.ToString().ToCharArray().Reverse()) Console.Write($"{Digits}, ");
             Console.ReadKey();
 
             //Задача №2
             Console.ForegroundColor = ConsoleColor.Green;Console.WriteLine("\n\t\tЗадача №2");Console.ResetColor();
             int[] InputArr= new int[] { 1, 2, 3, 4, 5, -1, -2, -3, -14, -5 };
             int[] OutputArr = new int[2];
-            OutputArr[0] = InputArr.Where(Num=>Num>0).Count();
-            OutputArr[1] = InputArr.Where(Num => Num < 0).Sum();
-            //вводим на экран
-            Console.Write("InputArrey = ");
-            foreach (int Num in InputArr)Console.Write($"{Num}, ");
-            Console.Write("\nOutputArrey = ");
-            foreach (int Num in OutputArr) Console.Write($"{Num}, ");
+            OutputArr[0] = InputArr.Where(Num=>Num>0).Count();OutputArr[1] = InputArr.Where(Num => Num < 0).Sum();
+            Console.Write("InputArrey = ");foreach (int Num in InputArr)Console.Write($"{Num}, ");
+            Console.Write("\nOutputArrey = ");foreach (int Num in OutputArr) Console.Write($"{Num}, ");
             Console.ReadKey();
-
+           
             //Задача №3
             Console.ForegroundColor = ConsoleColor.Green;Console.WriteLine("\n\t\tЗадача №3");Console.ResetColor();
             string[] NameClubArr = new string[] { "Истиклол", "Худжанд", "Регар-Тадаз", "Равшан", "Хайр", "Сарой-Камар" };
             Console.Write("Arrey(до сортировка):  ");
             foreach (string NameClub in NameClubArr) Console.Write($"{NameClub}, ");
-            NameClubArr = NameClubArr.OrderBy(Name=>Name.Length).ToArray();
             Console.Write("\nArrey(после сортировка):  ");
-            foreach (string NameClub in NameClubArr) Console.Write($"{NameClub}, ");
+            foreach (string NameClub in NameClubArr.OrderBy(Name => Name.Length).ToArray()) Console.Write($"{NameClub}, ");
             Console.ReadKey();
-
+            
             //Задача №4
             Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("\n\t\tЗадача №4"); Console.ResetColor();
             int[] NumberArrey = new int[] { 1, 1, 1, 1, -5, 1, 1, 1 };
             Console.Write(" Массив: ");
             foreach (int Num in NumberArrey) Console.Write($"{Num}, ");
-            int Max = NumberArrey.Max(); int Min = NumberArrey.Min();
-            int UnikalElement= NumberArrey.Single(Numb=>NumberArrey.Count(N=>N==Max)==1 && Numb==Max || NumberArrey.Count(N => N == Min) == 1 && Numb == Min);
-            Console.Write($"\n Уникалный елемент массив: {UnikalElement}");
+            Console.Write($"\n Уникалный елемент массив: {NumberArrey.Single(Numb => NumberArrey.Count(N => N == NumberArrey.Max()) == 1 && Numb == NumberArrey.Max() || NumberArrey.Count(N => N == NumberArrey.Min()) == 1 && Numb == NumberArrey.Min())}");
             Console.ReadKey();
         }
     }
